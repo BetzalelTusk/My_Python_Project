71b5e6061dfc5f1adacff17016406f4898417b80
betzaleltusk <betzaleltusk@gmail.com>
2025-06-08 21:57:28 -0400

    GUI - POWER ON!

---
+++ To-Do_List.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("To-Do List")
+root.geometry("400x400")
+
+tasks = []
+
+
+def add_task():
+    task = entry.get()
+    if task.strip():
+        tasks.append(task)
+        update_listbox()
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Input Error", "Please enter a task.")
+
+
+def delete_task():
+    try:
+        selected_index = listbox.curselection()[0]
+        tasks.pop(selected_index)
+        update_listbox()
+    except IndexError:
+        messagebox.showwarning(
+            "Selection Error", "Please select a task to delete.")
+
+
+def update_listbox():
+    listbox.delete(0, tk.END)
+    for task in tasks:
+        listbox.insert(tk.END, tasks)
+
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=10)
+
+add_button = tk.Button(root, text="add task", command=add_task)
+add_button.pack(pady=10)
+
+listbox = tk.Listbox(root, width=10)
+listbox.pack(pady=10)
+
+delete_button = tk.Button(root, text="delete button", command=delete_task)
+delete_button.pack(pady=10)
+
+root.mainloop()
